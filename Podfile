a0d0b332e19e4502f32cf18f3ecbf318add97ec7
Trevor Beasty <trevor.beasty@weightwatchers.com>
2019-03-04 10:11:55 -0500

    set up pods

---
+++ Podfile
@@ -0,0 +1,6 @@
+platform :ios, '10.0'
+use_frameworks!
+
+target 'OASIS' do
+  pod 'RxSwift',    '~> 4.0'
+end
---
+++ Pods/Target Support Files/Pods-OASIS/Pods-OASIS-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_OASIS : NSObject
+@end
+@implementation PodsDummy_Pods_OASIS
+@end
