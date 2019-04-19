44bedf8a5b645b3e38f9a50d0012b390cb3453a4
Trevor Beasty <trevor.beasty@weightwatchers.com>
2019-04-19 08:26:55 -0400

    reducer store

--- Pod/Classes/ReducerStore.swift
+++ Pod/Classes/ReducerStore.swift
@@ -8,6 +8,15 @@
 import Foundation
 import RxSwift
 
+// Ultimately, I think this approach is not desirable.
+// 1 - In networking calls, you lose the context of a completion handler. That completion handler is now essentially
+// a 'unidirectional flow'. It's possible results are just discrete actions. This loss of context does not read well.
+// 2 - All this would really do for us is provide an easy way to mock 'agents'. B/c agents now partake in the action stream,
+// we could simulate their behavior by sending actions into the store. We would also be able to check that correct inputs are being sent
+// to the agent OR associate reactions with inputs (if this input, then send this action, etc).
+// 3 - We would now need to to map all service calls and other artifacts into actions. This would need to be tested.
+// 4 - It may be better to just provide a better way (from a testing standpoint) to inject services into a store.
+
 public class Agent<Input, Action>: Module<Input, Action> { }
 
 public class ReducerStore<State, Action, Output, AgentInput>: Module<Action, Output>, _StoreType {
